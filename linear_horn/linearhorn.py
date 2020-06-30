20d742c592cacf012c5a85926003819b313ab1c7
zzephaniahh <zeph@umich.edu>
2020-06-29 21:51:34 -0400

    changed clause to variable, and added example comments in linearhorn.py

--- linear_horn/linearhorn.py
+++ linear_horn/linearhorn.py
@@ -1,88 +1,76 @@
-# a b c
-# !a !b c
-# a !b !c
-# !a b !c
+# Zephaniah Hill
+# The University of Michigan -- EECS 598
+# June 28, 2018
 
 from pysat.solvers import Solver, Minisat22
 
 import numpy as np # used to generate a list of integers
-#p wcnf 3 6 4
-#numb_var numb_clause
 
 
-class Linear_horn_clause_emperically_shown:
-    def __init__(self, numb_of_clauses):#, file_name):
+class horn_clause_generator:
+    def __init__(self, numb_of_variables):#, file_name):
 
         #self.file_name = file_name
-        self.numb_of_clauses = numb_of_clauses + 1 # add 1 because np.arange is noninclusive.
+        self.numb_of_variables = numb_of_variables + 1 # add 1 because np.arange is noninclusive.
         self.endline = ' '+str(0) # added for readability in string formatting
         horn_formula = []
 
-    def generate_horn_clauses_pysat(self):
+    def build_using_pysat(self):
         #with open(self.filename, 'a') as f:
 
-        var_list =  np.arange(1,self.numb_of_clauses) # make a list of integers size = numb_of_clauses
+        var_list =  np.arange(1,self.numb_of_variables) # make a list of integers size = numb_of_variables
         horn_formula = []
-        for non_neg_var in var_list:
+        for non_neg_var in var_list: # outter loop for the non-negative variable in the horn variable
             subformula = []
-            subformula.append(non_neg_var)
-            for var in var_list:
-                if var != non_neg_var:
-                    subformula.append(-var)
-            horn_formula.append(subformula)
-            #print(horn_formula)
-        return horn_formula
-
-
+            subformula.append(non_neg_var) # save the non-negative variable to the subformula
+            for var in var_list: # inner loop to generate the N-1 negative variables
+                if var != non_neg_var: # exlude the non-negative veriable
+                    subformula.append(-var) # add each negative variable to the subformula
+            horn_formula.append(subformula) # add the subformula to the horn variable
+        return horn_formula # return a horn variable with all possible permutations on N variables
 
 
 
 
 def main():
 
-    clause_size_list = [10, 100, 1000]
-    avg_time_for_clause_size_set = []
-    for numb_of_clauses in clause_size_list:
-        #import pdb; pdb.set_trace()
-        linear_horn = Linear_horn_clause_emperically_shown(numb_of_clauses)
-        horn_formula = linear_horn.generate_horn_clauses_pysat()
+    variable_size_list = [10, 100, 1000] # each item in this list is used to generate a horn variable of that size
+    avg_time_for_variable_size_set = [] # this list is maintained to compare the time increase per formula size
+    for numb_of_variables in variable_size_list:
+        linear_horn = horn_variable_generator(numb_of_variables) # instance of
+        horn_formula = linear_horn.build_using_pysat() # generate a horn variable of size numb_of_variables
 
         avg_time = 0
-        numb_runs = 10
+        numb_runs = 10 # average the time to solve over numb_run runs of the sat solver.
         for i in range(1,numb_runs):
             with Minisat22(bootstrap_with=horn_formula, use_timer=True) as m:
-                m.solve()
+                m.solve() # solve using minisat
 
-                avg_time += m.time()
-        avg_time = avg_time/numb_runs
-        avg_time_for_clause_size_set.append(avg_time)
+                avg_time += m.time() # accumulate the time used in the solver
+        avg_time = avg_time/numb_runs # find the real average over numb_run minisat calls
+        avg_time_for_variable_size_set.append(avg_time) # maintain a list of average times for# [a, b, c]
 
-        print(avg_time_for_clause_size_set)
 
-    for index, time in enumerate(avg_time_for_clause_size_set[:-1]):
-        time_diff = avg_time_for_clause_size_set[index+1]/time
+    # this loop prints the time increase per formula
+    for index, time in enumerate(avg_time_for_variable_size_set[:-1]):
+        time_diff = avg_time_for_variable_size_set[index+1]/time
 
-        print "\n" "It takes ", time_diff, " times longer to compute", clause_size_list[index+1], " clauses than ", clause_size_list[index], " clauses"
+        print "\n" "It takes ", time_diff, " times longer to compute", variable_size_list[index+1], " variables than ", variable_size_list[index], " variables"
+
+        #TODO: change to variables
+
 
 if __name__ == '__main__':
     main()
 
 
+# Example: for three varibles (variable_list_size = 3)
+# Variables = [a, b, c] = [1, 2, 3]
+#
+# horn_clause:
+# (~a + ~b + c) * (~a + b + ~c) * (a + ~b + ~c)
 
-
-# def generate_horn_clauses_DIMACS(self):
-#     with open(self.filename, 'a') as f:
-#
-#         var_list =  np.arange(1,self.numb_of_clauses) # make a list of integers size = numb_of_clauses
-#
-#         f.write(DIMACS_string_all_var_term[1:] + "\n") # add the final formula and end the line
-#
-#         for var in var_list:
-#             DIMACS_string = ""
-#             nonneg = var
-#             for var in var_list:
-#                 if var != nonneg:
-#                     DIMACS_string += "-" + str(var) + " "
-#             DIMACS_string += str(nonneg) + self.endline
-#             print(DIMACS_string)
-#     return horn_formula
+# horn_clauses_DIMACS:
+# -1 -2 3 0
+# -1 2 -3 0
+# 1 -2 -3 0
